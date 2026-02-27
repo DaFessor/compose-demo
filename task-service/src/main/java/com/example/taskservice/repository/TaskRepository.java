c6167fe4adcf9223e7e544560c029bc36874f609
Steen Krøyer <stek@via.dk>
2026-02-27 12:56:54 +0000

    Removed warninngs abou NonNull

--- task-service/src/main/java/com/example/taskservice/repository/TaskRepository.java
+++ task-service/src/main/java/com/example/taskservice/repository/TaskRepository.java
@@ -1,18 +1,20 @@
 package com.example.taskservice.repository;
 
-import com.example.taskservice.entity.Task;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
+import com.example.taskservice.entity.Task;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
-    Optional<Task> findByName(String name);
+    Optional<@NonNull Task> findByName(@NonNull String name);
 
     @Query("SELECT t FROM Task t WHERE LOWER(t.name) LIKE LOWER(CONCAT('%', :searchTerm, '%'))")
     List<Task> searchByName(@Param("searchTerm") String searchTerm);
